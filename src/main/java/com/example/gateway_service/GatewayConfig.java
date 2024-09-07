86d0596055d0813a5dde32efbbee96d1bbe1f2be
DanielRodado <d4nielrodado@gmail.com>
2024-09-06 21:01:49 -0500

    Filtros de seguridad agregados al enrrutado de peticiones

--- src/main/java/com/example/gateway_service/GatewayConfig.java
+++ src/main/java/com/example/gateway_service/GatewayConfig.java
@@ -1,5 +1,7 @@
 package com.example.gateway_service;
 
+import com.example.gateway_service.configurations.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,11 +10,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public RouteLocator router(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("userservice", r -> r.path("/api/users/**").uri("lb://user-service"))
-                .route("taskservice", r -> r.path("/api/tasks/**").uri("lb://task-service"))
+                .route("userservice", r -> r.path("/api/users/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
+                        .uri("lb://user-service"))
+                .route("userservice", r -> r.path("/api/auth/**")
+                        .uri("lb://user-service"))
+                .route("taskservice", r -> r.path("/api/tasks/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
+                        .uri("lb://task-service"))
                 .build();
     }
 
--- src/main/java/com/example/gateway_service/configurations/SecurityConfig.java
+++ src/main/java/com/example/gateway_service/configurations/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.gateway_service.configurations;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+@Configuration
 public class SecurityConfig {
 
     @Bean
