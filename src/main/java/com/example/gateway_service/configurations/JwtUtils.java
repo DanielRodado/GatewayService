17c02e563ae08e72670aad85d0ce5f3d3b24ba2a
DanielRodado <d4nielrodado@gmail.com>
2024-09-06 19:57:33 -0500

    Métodos de validación de token (JWT)

--- src/main/java/com/example/gateway_service/configurations/JwtUtils.java
+++ src/main/java/com/example/gateway_service/configurations/JwtUtils.java
@@ -1,4 +1,48 @@
 package com.example.gateway_service.configurations;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
 public class JwtUtils {
+
+    private final SecretKey secretKey;
+
+    private String secret = System.getenv("SECRET_KEY");
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public JwtUtils() {
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(this.secret));
+    }
+
+    public String extractUsername(String token) {
+        return parseClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        final String tokenUsername = extractUsername(token);
+        return (!isTokenExpired(token));
+    }
+
+    private Claims parseClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return parseClaims(token).getExpiration().before(new Date());
+    }
+
 }
